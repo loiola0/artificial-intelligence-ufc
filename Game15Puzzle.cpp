2009ebdbb52ee1a55352ecfebbf1e177e64eca9d
luisfelipe23 <luisfelipedomingosvieira@gmail.com>
2021-11-07 20:07:03 -0300

    Bug fix

--- Game15Puzzle.cpp
+++ Game15Puzzle.cpp
@@ -8,21 +8,17 @@
 #include <vector>
 #include <queue>
 #include <functional>
+#include <locale.h>
 #include <unistd.h>
 
 using namespace std;
 
-
 class Node;
 
-
-
 bool idfs();
-bool greedy_misplaced();
-bool A_star_Manhattan();
-bool A_star_misplaced();
-bool ldfs(int k);
-bool greedy_Manhattan();
+bool A_estrela();
+bool dfs(int k);
+bool gulosa();
 bool bfs();
 
 void read();
@@ -30,23 +26,21 @@ void read();
 
 string print(Node *a);
 
-bool   hassolution();
-int    checkinv(int v[]);
-int    myswap(int x);
-int    Manhattan (Node *x);
-int    misplaced (Node *x);
+bool temSolucao();
+int contaInversoes(int v[]);
+int meuSwap(int x);
+int Manhattan (Node *x);
 
 // para melhor desempenho em heurísticas
-void   creat_dicf(); 
+void   criaDic();
 
-string myhash(int v[]);
-bool   eq(int a[],int b[]);
-bool   nodeEq(Node *a, Node *b);
-void   cp(int a[],int b[]);
+string meuHash(int v[]);
+bool igual(int a[],int b[]);
+bool nosIguais(Node *a, Node *b);
+void copiar(int a[],int b[]);
 
-
-int parseLine(char* line);
-int getValue();
+int analisaLinha (char* linha);
+int obterValor();
 
 
 //Movimentação necessária
@@ -57,79 +51,78 @@ pair<int,int> tr[] = {make_pair(-1, 0),
 
 
 int ar_i[16],z_i[2],ar_f[16],z_f[2];
-vector<int> finalpos(16);
+vector<int> posFinal(16);
 map<string,int> setx;
 
 int main(){
-	
+    setlocale(LC_ALL, "Portuguese");
 	read();
-	creat_dicf();
-	
-	
-	if(!hassolution()){
+	criaDic();
+
+
+	if(!temSolucao()){
 		printf("Sem solução!\n");
 		return 0;
 	}
-	
+
 	//Procurando a solução para o puzzle
 	int start_s=clock();
 	printf("Procurando solução..\n");
-	
-	
+
+
 	//Escolha qual algoritmo quer rodar e fazer o teste, basta descomentar para executa-lo
-	// bool ok = ldfs(15);
+	bool ok = dfs(15);
 	// bool ok = bfs();
 	// bool ok = idfs();
-	// bool ok = greedy_Manhattan();
-	// bool ok = A_star_Manhattan();	
-	bool ok = A_star_Manhattan();
-	
+	// bool ok = gulosa();
+	// bool ok = A_estrela();
+
 	if(!ok){
 		//No caso de não possui solução
-		printf("No solution\n");
+		printf("Sem solução\n");
 	}
 	else{
 		// Fornece informações de tempo e memória gastos para encontrar a solução
 		int stop_s=clock();
 		cout << "Tempo: " << (stop_s-start_s)/double(CLOCKS_PER_SEC)*1000 << endl;
-		printf("Memoria gasta: %dKB\n",getValue());
-		
+		printf("Memoria gasta: %dKB\n",obterValor());
+
 	}
 	return 0;
 }
 
-/****Class****/
+/****Classe****/
 class Node{
-	
+
 	public:
 	int array[16]; //tamango do puzzle 4x4
-	int zero[2];   //position of zero {i, j}
-	int depth;     //profundidade
-	string path;   
-	
+	int zero[2];   //posicao do zero {i, j}
+	int profundidade;     //profundidade
+	string caminho; //caminho percorrido após uma movimentação
+
 	//descontructor
 	~Node(){
-		
+
 	}
-	
-	//constructor
-	Node(int array[],int zero[], int depth, string path){
-		cp(array,this->array);
+
+	//construtor
+	Node(int array[],int zero[], int profundidade, string caminho){
+		copiar(array, this->array);
 		this->zero[0]=zero[0];
 		this->zero[1]=zero[1];
-		this->depth = depth;
-		if(depth == 0){
-			this->path = print(this);
+		this->profundidade = profundidade;
+		if(profundidade == 0){
+			this->caminho = print(this);
 		}
 		else{
-			this->path = path + print(this);
+			this->caminho = caminho + print(this);
 		}
 	}
-	
+
 
 	vector<Node*> make_desc(){
 		int i,j;
-		
+
 		vector<Node*> l;
 		for(int k=0;k<4;k++){
 			int c[16],t[2];
@@ -137,23 +130,23 @@ class Node{
 			i=this->zero[0]+tr[k].first;
 			j=this->zero[1]+tr[k].second;
 			if(i>=0 && i<4 && j>=0 && j<4){
-				
-				cp(this->array,c);
+
+				copiar(this->array,c);
 				c[this->zero[0]*4+this->zero[1]]=c[i*4+j];
-				
+
 				c[i*4+j]=0;
 				t[0]=i;
 				t[1]=j;
-				
+
 				//verifique se o nó já foi visto, se não foi visto, adicione
 				//se for visto, mas a profundidade for menor do que a anterior, adicione também
-				if(setx.find(myhash(c))==setx.end()){
-					Node *tt = new Node(c,t,this->depth+1,this->path);
+				if(setx.find(meuHash(c))==setx.end()){
+					Node *tt = new Node(c,t,this->profundidade+1,this->caminho);
 					l.push_back(tt);
 				}
-				else if(setx[myhash(c)]>this->depth+1){
-					setx[myhash(c)]=this->depth+1;
-					Node *tt = new Node(c,t,this->depth+1,this->path);
+				else if(setx[meuHash(c)]>this->profundidade+1){
+					setx[meuHash(c)]=this->profundidade+1;
+					Node *tt = new Node(c,t,this->profundidade+1,this->caminho);
 					l.push_back(tt);
 				}
 			}
@@ -165,8 +158,8 @@ class Node{
 //Algoritmos
 
 //Depth first search(DFS) limited by k (Busca em profunidade com limitação)
-bool ldfs(int k){
-	
+bool dfs(int k){
+
 	Node *end = new Node(ar_f,z_f,0,"");
 	Node *start = new Node(ar_i,z_i,0,"");
 	vector<Node*> l;
@@ -178,24 +171,24 @@ bool ldfs(int k){
 		flag=0;
 		t = l.back();
 		l.pop_back();
-		
-		if(setx.find(myhash(t->array))==setx.end())
-			setx.insert(make_pair(myhash(t->array),t->depth));
-		
-		else if(setx[myhash(t->array)]>=t->depth){
-			setx[myhash(t->array)]=t->depth;
+
+		if(setx.find(meuHash(t->array))==setx.end())
+			setx.insert(make_pair(meuHash(t->array),t->profundidade));
+
+		else if(setx[meuHash(t->array)]>=t->profundidade){
+			setx[meuHash(t->array)]=t->profundidade;
 		}
-		
+
 		else{
 			flag=1;
 		}
-		
-		if(nodeEq(t,end)){
-			cout << t->path;
-			printf("Depth: %d\n",t->depth);
+
+		if(nosIguais(t,end)){
+			cout << t->caminho;
+			printf("Profundidade: %d\n",t->profundidade);
 			return true;
 		}
-		if(t->depth<k && !flag){
+		if(t->profundidade<k && !flag){
 			tt = t->make_desc();
 			for(int i=0;i<(int)tt.size();i++){
 				l.push_back(tt[i]);
@@ -208,7 +201,7 @@ bool ldfs(int k){
 
 //Breath first search(Busca por largura)
 bool bfs(){
-	
+
 	Node *end = new Node(ar_f,z_f,0,"");
 	Node *start = new Node(ar_i,z_i,0,"");
 	queue<Node*> l;
@@ -220,25 +213,25 @@ bool bfs(){
 		flag = 0;
 		t = l.front();
 		l.pop();
-		
-		if(setx.find(myhash(t->array))==setx.end())
-			setx.insert(make_pair(myhash(t->array),t->depth));
-		
-		else if(setx[myhash(t->array)]>=t->depth){
-			setx[myhash(t->array)]=t->depth;
+
+		if(setx.find(meuHash(t->array))==setx.end())
+			setx.insert(make_pair(meuHash(t->array),t->profundidade));
+
+		else if(setx[meuHash(t->array)]>=t->profundidade){
+			setx[meuHash(t->array)]=t->profundidade;
 		}
-		
+
 		else{
 			flag=1;
 		}
-		
-		if(nodeEq(t,end)){
-			cout << t->path;
-			printf("Depth: %d\n",t->depth);
+
+		if(nosIguais(t,end)){
+			cout << t->caminho;
+			printf("Profundidade: %d\n",t->profundidade);
 			return true;
 		}
-		if(t->depth<80 && !flag){
-			tt = t->make_desc();			
+		if(t->profundidade<80 && !flag){
+			tt = t->make_desc();
 			for(int i=0;i<(int)tt.size();i++){
 				l.push(tt[i]);
 			}
@@ -250,9 +243,9 @@ bool bfs(){
 
 //Iterative Depth first search(Busca iterativa por profunidade)
 bool idfs(){
-	
+
 	for(int i=0;i<80;i++){
-		if(ldfs(i)){
+		if(dfs(i)){
 			return true;
 		}
 		setx.clear();
@@ -267,86 +260,86 @@ struct f{
 };
 
 //Busca Gulosa Heurística Manhattan
-bool greedy_Manhattan(){
-	
+bool gulosa(){
+
 	Node *end = new Node(ar_f,z_f,0,"");
 	Node *start = new Node(ar_i,z_i,0,"");
 	priority_queue< pair<int,Node*>, vector <pair<int,Node*> > , f > pq;
-	pq.push (make_pair(0,start));
-	Node *current_node;
+	pq.push (make_pair(0, start));
+	Node *noAtual;
 	int flag;
 	while (!pq.empty()) {
 		flag = 0;
-		current_node = pq.top().second;
+		noAtual = pq.top().second;
 		pq.pop();
-		
-		if(setx.find(myhash(current_node->array))==setx.end())
-			setx.insert(make_pair(myhash(current_node->array),current_node->depth));
-		
-		if(setx[myhash(current_node->array)]>=current_node->depth){
-			setx[myhash(current_node->array)]=current_node->depth;
+
+		if(setx.find(meuHash(noAtual->array))==setx.end())
+			setx.insert(make_pair(meuHash(noAtual->array),noAtual->profundidade));
+
+		if(setx[meuHash(noAtual->array)]>=noAtual->profundidade){
+			setx[meuHash(noAtual->array)]=noAtual->profundidade;
 		}
-		
+
 		else{
 			flag=1;
 		}
-		
-		if(nodeEq(current_node,end)){
-			cout << current_node->path;
-			printf("Depth: %d\n",current_node->depth);
+
+		if(nosIguais(noAtual,end)){
+			cout << noAtual->caminho;
+			printf("Profundidade: %d\n",noAtual->profundidade);
 			return true;
 		}
 		if(!flag){
-			vector<Node*> dsc = current_node->make_desc();
+			vector<Node*> dsc = noAtual->make_desc();
 			for (int i=0; i<(int)dsc.size();i++) {
 				pq.push(make_pair(Manhattan(dsc[i]),dsc[i]));
 			}
 		}
-		delete current_node;
+		delete noAtual;
 	}
 	return false;
 }
 
 
 
-//Busca gulosa com heurística Manhattan
-bool A_star_Manhattan () {
-	//initialize nodes start and end
+//Busca A* com heurística Manhattan
+bool A_estrela () {
+	//inicializa os nós do início e do fim
 	Node *end = new Node(ar_f,z_f,0,"");
 	Node *start = new Node(ar_i,z_i,0,"");
 	//start->cost = 0;
 	priority_queue< pair<int,Node*>, vector <pair<int,Node*> > , f > pq;
-	pq.push (make_pair(0,start));
-	Node *current_node;
+	pq.push (make_pair(0, start));
+	Node *noAtual;
 	int flag;
 	while (!pq.empty()) {
 		flag = 0;
-		current_node = pq.top().second;
+		noAtual = pq.top().second;
 		pq.pop();
-		
-		if(setx.find(myhash(current_node->array))==setx.end())
-			setx.insert(make_pair(myhash(current_node->array),current_node->depth));
-		
-		if(setx[myhash(current_node->array)]>=current_node->depth){
-			setx[myhash(current_node->array)]=current_node->depth;
+
+		if(setx.find(meuHash(noAtual->array))==setx.end())
+			setx.insert(make_pair(meuHash(noAtual->array),noAtual->profundidade));
+
+		if(setx[meuHash(noAtual->array)]>=noAtual->profundidade){
+			setx[meuHash(noAtual->array)]=noAtual->profundidade;
 		}
-		
+
 		else{
 			flag=1;
 		}
-		
-		if(nodeEq(current_node,end)){
-			cout << current_node->path;
-			printf("Depth: %d\n",current_node->depth);
+
+		if(nosIguais(noAtual,end)){
+			cout << noAtual->caminho;
+			printf("Profundidade: %d\n", noAtual->profundidade);
 			return true;
 		}
 		if(!flag){
-			vector<Node*> dsc = current_node->make_desc();
+			vector<Node*> dsc = noAtual->make_desc();
 			for (int i=0; i<(int)dsc.size();i++) {
-				pq.push(make_pair(Manhattan(dsc[i])*1.1+dsc[i]->depth,dsc[i]));
+				pq.push(make_pair(Manhattan(dsc[i])*1.1+dsc[i]->profundidade,dsc[i]));
 			}
 		}
-		delete current_node;
+		delete noAtual;
 	}
 	return false;
 }
@@ -392,34 +385,34 @@ string print(Node *a){
 }
 
 //Funções auxiliares
-//verifique se a configuração pode ser resolvida 
-bool hassolution(){
+//verifique se a configuração pode ser resolvida
+bool temSolucao(){
 	Node *end = new Node(ar_f,z_f,0,"");
 	Node *start = new Node(ar_i,z_i,0,"");
 	int inv_i,inv_f,br_i,br_f;
-	inv_i = checkinv(start->array);
-	inv_f = checkinv(end->array);
-	br_i  = myswap(start->zero[0]);
-	br_f  = myswap(end->zero[0]);
+	inv_i = contaInversoes(start->array);
+	inv_f = contaInversoes(end->array);
+	br_i  = meuSwap(start->zero[0]);
+	br_f  = meuSwap(end->zero[0]);
 	if (((inv_i % 2 == 0) == (br_i % 2 == 1)) == ((inv_f % 2 == 0) == (br_f % 2 == 1)))
 		return true;
-	return false; 
+	return false;
 }
 
 //contar o número de inversões
-int checkinv(int v[]){
-	int sum=0;
+int contaInversoes(int v[]){
+	int som=0;
 	for(int i=0;i<16;i++){
 		for(int j=i+1;j<16;j++){
 			if(v[i]>v[j] && v[i]!=0 && v[j]!=0)
-				sum+=1;
+				som+=1;
 		}
 	}
-	return sum;
+	return som;
 }
 
 //muda x, uma vez que foi contado do fundo, começando em 1, para verificar a solubilidade
-int myswap(int x){
+int meuSwap(int x){
 	switch(x){
 		case 3:return 1;
 		case 2:return 2;
@@ -434,7 +427,7 @@ int Manhattan (Node *x) {
   int acc = 0;
   for (int i=0; i<16; i++) {
     if (x->array[i]!=0) {
-      int index= finalpos[x->array[i]];
+      int index= posFinal[x->array[i]];
       int xf = index/4;
       int yf = index%4;
       int xa = i/4;
@@ -448,63 +441,63 @@ int Manhattan (Node *x) {
 
 // para melhor performance calculando as eurísticas, cria uma espécie de dicionário
 // para um dado bloco k, o índice é pos final [k]
-void creat_dicf() {
-  for (int i=0; i<16;i++) {
+void criaDic() {
+  for (int i=0; i < 16; i++) {
     int g = ar_f[i];
-    finalpos[g]=i;
+    posFinal[g]=i;
   }
 }
 
 //retorna o array como uma string
-string myhash(int v[]){
+string meuHash(int v[]){
 	string s = "";
-	for(int  i =0;i<16;i++)
-		s+=to_string(v[i]);
+	for(int  i =0; i<16; i++)
+		s += to_string(v[i]);
 	return s;
 }
 
 //verifique se 2 matrizes de configurações são iguais
-bool eq(int a[], int b[]){
-	for(int i=0;i<16;i++)
-		if(a[i]!=b[i]) return false;
+bool igual(int a[], int b[]){
+	for(int i = 0; i < 16; i++)
+		if(a[i] != b[i]) return false;
 	return true;
 }
 
 //verifica se dois nós são iguais
-bool nodeEq(Node *a, Node *b){
-	if(a && b)return eq(a->array,b->array);
+bool nosIguais(Node *a, Node *b){
+	if(a && b)return igual(a->array, b->array);
 	return false;
 }
 
 //copia o array para o array b
-void cp(int a[], int b[]){
+void copiar(int a[], int b[]){
 	for(int i=0;i<16;i++){
 		b[i]=a[i];
 	}
 }
 
 
-int parseLine(char* line){
-    
-    int i = strlen(line);
-    const char* p = line;
+int analisaLinha (char* linha){
+
+    int i = strlen(linha);
+    const char* p = linha;
     while (*p <'0' || *p > '9') p++;
-    line[i-3] = '\0';
+    linha[i-3] = '\0';
     i = atoi(p);
     return i;
 }
- 
-int getValue(){
-    FILE* file = fopen("/proc/self/status", "r");
-    int result = -1;
-    char line[128];
 
-    while (fgets(line, 128, file) != NULL){
-        if (strncmp(line, "VmRSS:", 6) == 0){
-            result = parseLine(line);
+int obterValor(){
+    FILE* file = fopen("/proc/self/status", "r");
+    int resultado = -1;
+    char linha[128];
+
+    while (fgets(linha, 128, file) != NULL){
+        if (strncmp(linha, "VmRSS:", 6) == 0){
+            resultado = analisaLinha(linha);
             break;
         }
     }
     fclose(file);
-    return result;
+    return resultado;
 }
